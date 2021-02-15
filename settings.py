404dd084d964e4c1374622a512502b327967c5dc
Joao <joao.g.leal@gmail.com>
2021-02-15 13:46:28 -0300

    removed unnecessary variables

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-APP_SECRET = os.environ.get('APP_SECRET')
 USER_DB = os.path.abspath(os.environ.get('USER_DATABASE'))
 CREDENTIALS_DB = os.path.abspath(os.environ.get('CREDENTIALS_DATABASE'))
 SALT = os.environ.get('SALT').encode()
